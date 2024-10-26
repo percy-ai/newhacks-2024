5e680691bb82b28ea31320a9c7032f9a7de74da5
daniel-wang-design <daniel.wang04@hotmail.com>
2024-10-26 17:59:04 -0400

    added call screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Voicemail from "./voicemail/voicemail";
 import Numpad from "./numpad/numpad";
 import RealTimeComponent from "./websockets/websockets";
+import Incall from "./incall/incall";
 
 function App() {
   return (
@@ -15,7 +16,11 @@ function App() {
             {/* <Route path="/incoming" element={<Incoming></Incoming>}></Route> */}
             <Route path="/voicemail" element={<Voicemail></Voicemail>}></Route>
             <Route path="/" element={<Numpad></Numpad>}></Route>
-            <Route path="/real" element={<RealTimeComponent></RealTimeComponent>}></Route>
+            <Route
+              path="/real"
+              element={<RealTimeComponent></RealTimeComponent>}
+            ></Route>
+            <Route path="/call" element={<Incall></Incall>}></Route>
           </Routes>
         </Router>
       </header>
---
+++ client/src/incall/incall.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import "./incall.css";
+
+const supabase: SupabaseClient = createClient(
+  "https://gldvgrcsiwscznrzcgxe.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdsZHZncmNzaXdzY3pucnpjZ3hlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk5NzYzMTQsImV4cCI6MjA0NTU1MjMxNH0.YAhY2yXpVLxRFErP2XS-4FhRGfFZ2BSgdSXuLdQql3U"
+);
+
+function Incall() {
+  const [isMuted, setIsMuted] = useState(true);
+
+  const toggleMute = async () => {
+    // Toggle the mute state
+    const newMuteState = !isMuted;
+    setIsMuted(newMuteState);
+
+    // Update Supabase
+    try {
+      const { error } = await supabase
+        .from("callstatus") // Replace with your actual table name
+        .upsert([{ id: 2, status: newMuteState }]); // Assuming you have an id column
+      if (error) throw error;
+    } catch (error) {
+      console.error("Error updating mute status:", error);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>{isMuted ? "Muted" : "Unmuted"}</h1>
+      <button onClick={toggleMute}>{isMuted ? "Unmute" : "Mute"}</button>
+    </div>
+  );
+}
+export default Incall;
