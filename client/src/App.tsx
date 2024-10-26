c6e210fb45934ca0805c0e00f89a4d663ea1a931
daniel-wang-design <daniel.wang04@hotmail.com>
2024-10-26 17:25:25 -0400

    temp

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Incoming from "./incoming/incoming";
-// import Voicemail from "./voicemail/voicemail";
+import Voicemail from "./voicemail/voicemail";
 import Numpad from "./numpad/numpad";
 import RealTimeComponent from "./websockets/websockets";
 
@@ -12,7 +12,7 @@ function App() {
       <header className="App-header">
         <Router>
           <Routes>
-            <Route path="/incoming" element={<Incoming></Incoming>}></Route>
+            {/* <Route path="/incoming" element={<Incoming></Incoming>}></Route> */}
             <Route path="/voicemail" element={<Voicemail></Voicemail>}></Route>
             <Route path="/" element={<Numpad></Numpad>}></Route>
             <Route path="/real" element={<RealTimeComponent></RealTimeComponent>}></Route>
