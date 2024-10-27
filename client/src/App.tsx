f7bdd6e4ca1ee5cb6cc5def90611726b08552594
daniel-wang-design <daniel.wang04@hotmail.com>
2024-10-27 09:10:05 -0400

    removed top bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,32 +13,33 @@ function App() {
   return (
     <div className="app">
       <Router>
-        <nav className="app" hidden={clicked}>
-          <ul>
-            <li>
-              <Link to="/numpad" onClick={(e) => setClicked(true)}>
-                Numpad
-              </Link>
-            </li>
-            <li>
-              <Link to="/incoming" onClick={(e) => setClicked(true)}>
-                incoming
-              </Link>
-            </li>
-            <li>
-              <Link to="/voicemail" onClick={(e) => setClicked(true)}>
-                voicemail
-              </Link>
-            </li>
-            <li>
-              <Link to="/accept" onClick={(e) => setClicked(true)}>
-                accept call
-              </Link>
-            </li>
-          </ul>
-        </nav>
         <Routes>
-          <Route path="/"></Route>
+          <Route path="/">
+            <nav className="app" hidden={clicked}>
+              <ul>
+                <li>
+                  <Link to="/numpad" onClick={(e) => setClicked(true)}>
+                    Numpad
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/incoming" onClick={(e) => setClicked(true)}>
+                    incoming
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/voicemail" onClick={(e) => setClicked(true)}>
+                    voicemail
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/accept" onClick={(e) => setClicked(true)}>
+                    accept call
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </Route>
           <Route path="/incoming" element={<Incoming></Incoming>}></Route>
           <Route path="/voicemail" element={<Voicemail></Voicemail>}></Route>
           <Route path="/numpad" element={<Numpad></Numpad>}></Route>
