9646d08f3d1473a2d24cb9f90793eae37a88e102
daniel-wang-design <daniel.wang04@hotmail.com>
2024-10-26 20:30:24 -0400

    added router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Incoming from "./incoming/incoming";
 import Voicemail from "./voicemail/voicemail";
 import Numpad from "./numpad/numpad";
@@ -8,22 +8,41 @@ import RealTimeComponent from "./websockets/websockets";
 import Incall from "./incall/incall";
 
 function App() {
+  const [clicked, setClicked] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <Router>
-          <Routes>
-            <Route path="/incoming" element={<Incoming></Incoming>}></Route>
-            <Route path="/voicemail" element={<Voicemail></Voicemail>}></Route>
-            <Route path="/" element={<Numpad></Numpad>}></Route>
-            <Route
-              path="/real"
-              element={<RealTimeComponent></RealTimeComponent>}
-            ></Route>
-            <Route path="/call" element={<Incall></Incall>}></Route>
-          </Routes>
-        </Router>
-      </header>
+    <div className="app">
+      <Router>
+        <nav className="app" hidden={clicked}>
+          <ul>
+            <li>
+              <Link to="/numpad" onClick={(e) => setClicked(true)}>
+                Numpad
+              </Link>
+            </li>
+            <li>
+              <Link to="/incoming" onClick={(e) => setClicked(true)}>
+                incoming
+              </Link>
+            </li>
+            <li>
+              <Link to="/voicemail" onClick={(e) => setClicked(true)}>
+                voicemail
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/"></Route>
+          <Route path="/incoming" element={<Incoming></Incoming>}></Route>
+          <Route path="/voicemail" element={<Voicemail></Voicemail>}></Route>
+          <Route path="/numpad" element={<Numpad></Numpad>}></Route>
+          <Route
+            path="/real"
+            element={<RealTimeComponent></RealTimeComponent>}
+          ></Route>
+          <Route path="/call" element={<Incall></Incall>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
